66757f404603c8d9aa3f20e94bde4f112dab6674
olexandr-harmash <olexandr.harmash@gmail.com>
2024-05-08 11:46:06 -0700

    fix to production

--- src/Estimation.API/Services/Abstractions/IEstimateService.cs
+++ src/Estimation.API/Services/Abstractions/IEstimateService.cs
@@ -1,4 +1,5 @@
 ﻿using diploma.Estimation.API.Dto;
+using static diploma.Estimation.API.Services.EstimationService;
 
 namespace diploma.Estimation.API.Services.Abstractions;
 
@@ -8,4 +9,5 @@ public interface IEstimateService
     Task<EstimateDto> CreateEstimate(EstimateDtoForCreate estimate);
     Task UpdateEstimate(Guid id, EstimateDtoForUpdate estimate, bool trackChanges);
     Task DeleteEstimate(Guid id, bool trackChanges);
+    Task<Qualification> MatchEstimateToPattern(Guid estimateId, bool trackChanges);
 }
--- src/Estimation.API/Services/EstimationServiceManager.cs
+++ src/Estimation.API/Services/EstimationServiceManager.cs
@@ -4,24 +4,20 @@ namespace diploma.Estimation.API.Services;
 
 public class EstimationServiceManager
 {
-    private readonly IEstimationService _estimationService;
     private readonly IEstimateService _estimateService;
     private readonly ICriterionService _criterionService;
     private readonly IEstimateCriterionService _estimateCriterionService;
 
     public EstimationServiceManager(
-        IEstimationService estimationService,
         IEstimateService estimateService, 
         ICriterionService criterionService,
         IEstimateCriterionService estimateCriterionService)
     {
-        _estimationService = estimationService;
         _estimateService = estimateService;
         _criterionService = criterionService;
         _estimateCriterionService = estimateCriterionService;
     }
 
-    public IEstimationService Estimation => _estimationService;
     public IEstimateService Estimate => _estimateService;
     public ICriterionService Criterion => _criterionService;
     public IEstimateCriterionService EstimateCriterion => _estimateCriterionService;
